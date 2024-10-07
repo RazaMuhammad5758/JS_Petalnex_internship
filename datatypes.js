88f91a4b5c64853319c318686b0bb2738f8083fa
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-07 17:24:44 +0500

    datatypes done

---
+++ datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all JS code as newer version
+
+// DATA TYPES in js 
+
+/* 
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+Object 
+*/
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+console.table([length, color, x, person, cars, date]);
