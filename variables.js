cc0c419ad4ed31b6f8bde9fe88b94a5de1566055
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-07 14:51:41 +0500

    variables done

---
+++ variables.js
@@ -0,0 +1,16 @@
+const Id = 1234     // const me value ko chjange nh kr skty
+let email = 'raza1234@gmail.com'        // changeable
+var password = 7714  // var is wja se use nhi krty q k var scope ko follow nahi krta is wja se let invent kia gaya
+city = 'karachi'
+let state;
+
+// Id = 111
+
+email = 'raza'
+password = 1111
+city = 'lahore'
+
+console.table([Id, email, password, city, state]);
+
+
+
