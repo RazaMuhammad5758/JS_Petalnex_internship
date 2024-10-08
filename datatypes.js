7ca7f69ddba4dcab0e351ba69abda82815da82f0
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-08 21:21:04 +0500

    operators

--- datatypes.js
+++ datatypes.js
@@ -35,3 +35,14 @@ const cars = ["Saab", "Volvo", "BMW"];
 const date = new Date("2022-03-25");
 
 console.table([length, color, x, person, cars, date]);
+
+
+
+// CONVERSION OF DATATYPE
+
+let score = 33
+console.log(typeof score);          // ab ye type me number hi btaega 
+
+let score_2 = Number("thirty");     // ab b tyoe me number hi btaega q k hum ne type convert krdi h
+console.log(typeof(score_2));
+
---
+++ operators.js
@@ -0,0 +1,10 @@
+/* ARTHEMATIC OPERATORS 
+
++,  - , / , *  
+
+used to perform operation on data*/
+
+let a = 10;
+let b = 5;
+
+console.table([ a+b, a-b, a*b, a/b])
