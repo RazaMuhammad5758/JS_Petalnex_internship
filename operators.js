dcf11cc6e296fc5b1d9d4069d4e382b907e41386
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-08 21:36:13 +0500

    unary operators

--- operators.js
+++ operators.js
@@ -1,10 +1,33 @@
 /* ARTHEMATIC OPERATORS 
 
-+,  - , / , *  
++,  - , / , *  , %(modulus),  **(exponential)
 
 used to perform operation on data*/
 
 let a = 10;
 let b = 5;
 
-console.table([ a+b, a-b, a*b, a/b])
+console.table([ a+b, a-b, a*b, a/b, a%b, a**b])
+
+
+/* UNARY OPERATORS 
+
+increment, decrement
+a++ = a + 1,            a-- = a - 1
+
+a++ (post increment)        a-- (post decrement)  pehly value print hogi usk bad next line me value change ho kr print hogi
+++a (pre increment)         --a (post decrement)  pehly value change hogi means 10 me 1 add ya sub hoga phr new value print hogi
+
+*/
+
+a++; // a ki value 10 h to us me 1 add ho kr 11 print hona chahye
+console.log(a);
+
+a--; // a ki value 10 h to us me 1 sub ho kr 9 print hona chahye
+console.log(a);
+
+
+/* ASSIGNMENT OPERATORS
+
+
+*/
