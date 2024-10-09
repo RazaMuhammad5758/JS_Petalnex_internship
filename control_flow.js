9d1b337a64f1822b2b0e87584077c8a98ff78e08
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-09 21:15:57 +0500

    if else done

--- control_flow.js
+++ control_flow.js
@@ -9,3 +9,30 @@ if(age>=18){
     console.log("you can't vote");
     
 }
+
+let num = 20;
+
+if (num%2 === 0){
+    console.log('Given number is even');
+    
+}else{
+    console.log('Given number is odd');
+    
+}
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+// ELSE IF 
+
+let color = 'pink';
+
+if (color === 'pink'){
+    console.log('color is light');
+    
+}else if(color === 'yellow'){
+    console.log('color is light');
+    
+}else{
+    console.log('color is dark');
+    
+}
