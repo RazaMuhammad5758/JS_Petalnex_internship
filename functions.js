691e761cf36216d6ddb42757a722bff043f69a9a
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-10 22:05:22 +0500

    functions

--- functions.js
+++ functions.js
@@ -1,5 +1,12 @@
-function addNum(num1, num2){
-    console.log(num1 + num2);
+// function addNum(num1, num2){
+//     console.log(num1 + num2);
     
+// }
+// addNum(5,2)
+
+function addNum(num1, num2){
+    return num1 + num2
 }
-addNum(5,2)
+const result = addNum(2,3)
+console.log(result);
+
