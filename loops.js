bb2db4e4ef096e2f18e59248d97b8f1f8e721afe
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-10 21:31:21 +0500

    functions

--- loops.js
+++ loops.js
@@ -1,13 +1,43 @@
 // FOR LOOP
 
-for(let i=0; i<10; i++){
-    console.log('Hellow', i);
+// for(let i=0; i<10; i++){
+//     console.log('Hellow', i);
     
-}
+// }
 
 // table of 2
 
-for(let i=2; i<21; i=i+2){
-    console.log(i);
+// for(let i=2; i<21; i=i+2){
+//     console.log(i);
     
+// }
+
+// even numbers print
+// for(let i=0; i<21; i++){
+//     if(i%2===0){
+//         console.log(i);
+        
+//     }
+// }
+
+// WHILE LOOP
+
+// let i = 0
+
+// while(i<15){
+//     console.log(i);
+//     i++
+    
+// }
+
+// DO WHILE LOOP
+
+let j = 0
+
+
+do{
+    console.log(j);
+    j++
+}
+while(j<15){
 }
