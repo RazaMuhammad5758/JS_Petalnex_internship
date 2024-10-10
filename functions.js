884e4adc08acc3595eef7cee16665be7bc1c512d
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-10 22:08:30 +0500

    functions

--- functions.js
+++ functions.js
@@ -4,9 +4,14 @@
 // }
 // addNum(5,2)
 
-function addNum(num1, num2){
-    return num1 + num2
-}
-const result = addNum(2,3)
-console.log(result);
+// function addNum(num1, num2){
+//     return num1 + num2
+// }
+// const result = addNum(2,3)
+// console.log(result);
 
+
+function loggedIn(username){
+    return `${username} just logged in`
+}
+console.log(loggedIn('raza'))
