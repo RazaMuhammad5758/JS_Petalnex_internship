499d05341a85bd1c0f4a9c4c6e72d2ba066a8f17
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-13 22:43:23 +0500

    map in array done

--- array_iteration.js
+++ array_iteration.js
@@ -19,3 +19,32 @@ function loop(value, index){
     console.log(index, value);
     
 }
+
+// MAP  map new array return krta h jis me hr element pr functionality apply hui hoti h 
+
+let ary = [5,10,15,20]
+
+let newAry = ary.map(function(value){
+    return value*10
+})
+
+console.log(newAry);
+
+
+const infoArray = [
+    {
+        firstName: "Raza",
+        lastName: "Muhammad"
+    },
+    {
+        firstName: "Moiz",
+        lastName: "Rehman"
+    }
+]
+
+const infoArray2 = infoArray.map(function(name){
+    return name.firstName + name.lastName
+})
+
+console.log(infoArray2);
+
