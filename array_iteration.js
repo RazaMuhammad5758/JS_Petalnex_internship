1d5ae108f7de61173637ec670ef9cb71c615cba9
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-13 21:49:36 +0500

    array iteration done

---
+++ array_iteration.js
@@ -0,0 +1,21 @@
+// FOR EACH LOOP
+
+// array  k elements ko separately print krta h
+
+const a = ['raza', 'moiz', 'noman', 'ibrahim']
+
+a.forEach(function(value, index){
+    console.log(index, value);
+    
+})
+
+// ANOTHER METHOD is this k function ko bahr b likh skty hen
+
+const b = ['raza', 'moiz', 'noman', 'ibrahim']
+
+b.forEach(loop)
+
+function loop(value, index){
+    console.log(index, value);
+    
+}
