e6d7c8061581c448dd7e033965bdfe31e9281a79
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-14 19:51:06 +0500

    object declaration and access

--- objects.js
+++ objects.js
@@ -7,3 +7,39 @@
 // | | ├── Object.values()
 // | | └── Object.entries()
 // | └── Object destructuring
+
+
+// object declaration
+
+const user = {
+    name: "raza",
+    email: "razamd7714@gmail.com",
+    age: 23,
+    location: "Karachi",
+    isloggedIn: false,
+    lastLoggedIn: ["monday", "saturday"]
+
+}
+
+// how to access object 
+
+console.log(user.name);
+console.log(user["email"]);  // is tarha b access kr skty hen agr object me email "email" ese likha ho to
+
+// how to edit object values
+
+user.age = 18
+
+console.log(user["age"]);
+
+// agr hum chahty hen k changes naa hon to object ko freeze b kr skty hen
+
+Object.freeze(user)
+
+user.name = "raza muhammad"
+console.log(user.name);
+
+
+
+
+
