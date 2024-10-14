b26000b2f3fb979fbadf313c1254a028a4798820
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-14 20:16:43 +0500

    object merge

--- objects.js
+++ objects.js
@@ -64,6 +64,14 @@ const newUser = {
 
 console.log(newUser.fullname.userFullName.firstname);
 
+// OBJECT MERGE
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+const obj3 = Object.assign(obj1, obj2)
+
+console.log(obj3);
 
 
 
