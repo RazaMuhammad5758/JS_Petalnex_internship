b2106687981b9ecd03d8d684ff389f12b640e8b9
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-16 21:18:35 +0500

    oject de structuring

---
+++ object_de_structuring.js
@@ -0,0 +1,11 @@
+const course = {
+    courseName: "Physics",
+    price: 1000,
+    courseInstructor: "Raza"
+}
+
+// ab object ki values print krwany k course.price ye b tariqa h pr ak or syntax b h
+
+const {price} = course
+
+console.log(price);
