77204e5e07f83bcb48f9fdb7238976277385b672
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-16 22:11:57 +0500

    promises resolve reject work

--- promises.js
+++ promises.js
@@ -13,6 +13,14 @@
 //promises ki 3 states hoti hen 
 //pending, fulfilled, rejected
 
-const promiseOne = new promise(function(resolve, reject){
+const promiseOne = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task is completed");
+        resolve()
+    }, 2000)
+})
+
+promiseOne.then(function(){
+    console.log("promise is consumed");
     
 })
