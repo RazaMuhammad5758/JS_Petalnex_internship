5976519b7e8dc611ae5c6bf20d57f31e94bcbb7d
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-16 22:05:51 +0500

    promises start

---
+++ promises.js
@@ -0,0 +1,18 @@
+// |── Promises
+// | ├── Promise states
+// | | ├── Pending
+// | | ├── Fulfilled
+// | | └── Rejected
+// | ├── Promise methods
+// | | ├── then()
+// | | ├── catch()
+// | | └── finally()
+// | └── Promise.all()
+
+
+//promises ki 3 states hoti hen 
+//pending, fulfilled, rejected
+
+const promiseOne = new promise(function(resolve, reject){
+    
+})
