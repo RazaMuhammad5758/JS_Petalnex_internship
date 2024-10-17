b9baf6a3ceac6826b50420be52e2be9267526ee2
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-17 20:17:34 +0500

    promises another way

--- promises.js
+++ promises.js
@@ -20,7 +20,37 @@ const promiseOne = new Promise(function(resolve, reject){
     }, 2000)
 })
 
+// resolve or then apis me connect hoty hen
+
 promiseOne.then(function(){
     console.log("promise is consumed");
+    
+})
+
+// another way of writing promise
+
+new Promise(function(resolve, reject){
+
+    setTimeout(function(){
+        console.log("new promise is complpeted");
+        resolve()
+    }, 2000)
+}).then(function(){
+    console.log("new promise is consumed");
+    
+})
+
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({
+            username: "Raza",
+            email: "razamd7714"
+        })
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
     
 })
