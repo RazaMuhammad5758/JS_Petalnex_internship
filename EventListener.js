0ccc27fce725a83f689075c9fc1c4256ca081b71
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-25 20:23:39 +0500

    event propagation

---
+++ EventListener.js
@@ -0,0 +1,8 @@
+// Event propagation
+
+// event bubbeling   bottom to top events apply hongy
+// event capturing   top to bottom events apply hongy
+
+// bubbeling example
+
+document.getElementById
