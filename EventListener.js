b5081299ec1823b282a8275e93da690e52075064
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-26 22:09:37 +0500

    event propagation

--- EventListener.js
+++ EventListener.js
@@ -5,4 +5,27 @@
 
 // bubbeling example
 
-document.getElementById
+// is me pehly inside event execcute hoga phr upper wala q k false likha h yahi chez bubbeling kehlati h
+
+// document.getElementById('images').addEventListener('click', function(){
+//     alert('images clicked')
+// }, false)
+
+// document.getElementById('sdCard').addEventListener('click', function(){
+//     alert('SD Card clicked')
+// }, false)
+
+
+// capturing example
+
+// is me pehly upper event execcute hoga phr lower wala q k true likha h yahi chez capturing kehlati h
+
+document.getElementById('images').addEventListener('click', function(){
+    alert('images clicked')
+}, true)
+
+document.getElementById('sdCard').addEventListener('click', function(){
+    alert('SD Card clicked')
+}, true)
+
+
