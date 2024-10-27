18435239adc6998f4a80bce00edf849f46e5bfdb
RazaMuhammad5758 <waistband161@gmail.com>
2024-10-27 20:35:07 +0500

    AJAX

--- AJAX.js
+++ AJAX.js
@@ -1,2 +1,4 @@
 // ajax is used to make 
 // ye react ki tarha kam krti h is me pura page reload ni hota bs sirf ak part reload hota h
+// is me sbse pehly request bheji jati h server ko XMLHTTPRequest 
+
