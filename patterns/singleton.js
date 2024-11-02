ff68537fbd2cd07d5a6f82fd5d8de50e1c3bb0ea
RazaMuhammad5758 <waistband161@gmail.com>
2024-11-02 20:49:04 +0500

    singleton patterns

--- patterns/singleton.js
+++ patterns/singleton.js
@@ -1,3 +1,5 @@
+const { connected } = require("process");
+
 class Singleton{
     constructor(){
         if(!Singleton.instance){
@@ -8,3 +10,42 @@ class Singleton{
 }
 const instance1 = new Singleton();
 const instance2 = new Singleton();
+
+console.log(instance1 === instance2);
+
+// example (database connection)
+
+
+class connectDatabase{
+    constructor(){
+        if(!connectDatabase.instance){
+            this.connection = this.connectTodatabase();
+            connectDatabase.instance = this;
+        }
+        return connectDatabase.instance;
+    }
+
+    connectTodatabase(){
+        console.log("connecting to database");
+        return { connected: true, connectionId: Math.random() }
+        
+    }
+
+    getConnection(){
+        return this.connection;
+    }
+}
+
+// Testing the Singleton behavior
+
+// First attempt to get the database connection
+const db1 = new connectDatabase();
+console.log("Database Connection 1:", db1.getConnection());
+
+// Second attempt to get the database connection
+const db2 = new connectDatabase();
+console.log("Database Connection 2:", db2.getConnection());
+
+// Check if db1 and db2 are the same
+console.log(db1 === db2); // true, because both are the same instance
+
