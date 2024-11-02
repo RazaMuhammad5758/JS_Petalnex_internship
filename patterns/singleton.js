dde6a750f23a1c408f43189c07a1522f007cb5e6
RazaMuhammad5758 <waistband161@gmail.com>
2024-11-02 20:57:43 +0500

    singleton patterns

--- patterns/singleton.js
+++ patterns/singleton.js
@@ -49,3 +49,48 @@ console.log("Database Connection 2:", db2.getConnection());
 // Check if db1 and db2 are the same
 console.log(db1 === db2); // true, because both are the same instance
 
+
+
+// EXAMPKLLE 2
+
+class logger{
+    constructor(){
+        if(!logger.instance){
+            this.logs = []
+            logger.instance = this;
+        }
+        return logger.instance
+    }
+
+    // Method to add a log entry
+    log(message){
+        const timeStamp = new Date().toISOString();
+        this.logs.push({message, timeStamp})
+        console.log(`[${timeStamp}] ${message}`);
+    }
+
+    // Method to get all logs
+    getLogs(){
+        return this.logs;
+    }
+
+
+    // Mthod to get the count of logs
+    logCount(){
+        return this.logs.length;
+    }
+}
+
+// testing
+
+const logger1 = new logger();
+logger1.log("User login successful")
+
+const logger2 = new logger();
+logger2.log("User requested account details");
+
+console.log("Total logs:", logger1.logCount());
+
+// Checking if logger1 and logger2 are the same instance
+console.log(logger1 === logger2); // true
+console.log("All logs:", logger1.getLogs());
